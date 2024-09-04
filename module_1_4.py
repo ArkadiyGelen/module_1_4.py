43a9d481259dea7188ae1fb75f3f45f4694e2795
ArkadiyGelen <ArkadijTolstov@gmail.com>
2024-09-04 14:41:49 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input("Введите словочочетпние:", ) #переменная
+print(len(my_string)) #колисество символов в строке
+print(my_string .lower()) #нижний реестор
+print(my_string .upper()) #верхний реестор
+print(my_string .replace(" ", "")) #замена симвалов
+print(my_string [0]) #вывод первого символа
+print(my_string [-1]) #вывод последнего символа
